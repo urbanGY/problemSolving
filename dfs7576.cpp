61ae6e2df70ca7ca04b42e56d1cc4acf27e18feb
DESKTOP-BEAR877\sfsfk <sfsfkj@gmail.com>
2019-03-06 00:54:43 +0900

    백준 DFS 7576 토마토 정답버전

--- dfs7576.cpp
+++ dfs7576.cpp
@@ -1,80 +1,81 @@
 #include <iostream>
+#include <stdlib.h>
 #include <queue>
 using namespace std;
 
-class node{
-private:
+typedef struct Node{
   int x;
   int y;
   int day;
-public:
-  node(int x, int y, int day)
-  :x(x),y(y),day(day)
-  {
+}Node;
 
-  }
-  int getX(){
-    return this->x;
-  }
-  int getY(){
-    return this->y;
-  }
-  int getDay(){
-    return this->day;
-  }
-};
+int board[1003][1003];
+queue< Node* > Q;
 
 int main(){
-  int M,N;
+  int M,N, max = 0;
   scanf("%d %d",&M,&N);
-  int board[N][M];
   for(int i = 0 ; i < N ; i++){
     for(int j = 0 ; j < M ; j++){
       scanf("%d",&board[i][j]);
-    }
-  }
-  node *pos;
-  queue< node* > Q;
-  for(int i = 0 ; i < N ; i++){
-    for(int j = 0 ; j < M ; j++){
       if(board[i][j] == 1){
-        Q.push(new node(i,j,0));
+        Node * node = (Node*)malloc(sizeof(Node));
+        node->x = i;
+        node->y = j;
+        node->day = 1;
+        Q.push(node);
       }
     }
   }
+  Node *pos;
   while(!Q.empty()){
     pos = Q.front();
-    int x = pos->getX();
-    int y = pos->getY();
-    int day = pos->getDay();
-    delete pos;
     Q.pop();
-    day++;
-    board[x][y] = day;
+    int x = pos->x;
+    int y = pos->y;
+    int day = pos->day;
+    free(pos);
+    if(max < day) max = day;
 
     if(x-1 >= 0 && board[x-1][y] == 0){
-      Q.push(new node(x-1,y,day));
+      board[x-1][y] = day + 1;
+      Node * node = (Node*)malloc(sizeof(Node));
+      node->x = x-1;
+      node->y = y;
+      node->day = day+1;
+      Q.push(node);
     }//북
     if(x+1 < N && board[x+1][y] == 0){
-      Q.push(new node(x+1,y,day));
+      board[x+1][y] = day + 1;
+      Node * node = (Node*)malloc(sizeof(Node));
+      node->x = x+1;
+      node->y = y;
+      node->day = day+1;
+      Q.push(node);
     }//남
     if(y-1 >= 0 && board[x][y-1] == 0){
-      Q.push(new node(x,y-1,day));
+      board[x][y-1] = day + 1;
+      Node * node = (Node*)malloc(sizeof(Node));
+      node->x = x;
+      node->y = y-1;
+      node->day = day+1;
+      Q.push(node);
     }//서
     if(y+1 < M && board[x][y+1] == 0){
-      Q.push(new node(x,y+1,day));
+      board[x][y+1] = day + 1;
+      Node * node = (Node*)malloc(sizeof(Node));
+      node->x = x;
+      node->y = y+1;
+      node->day = day+1;
+      Q.push(node);
     }//동
   }
-  int max = 0;
   for(int i = 0 ; i < N ; i++){
     for(int j = 0 ; j < M ; j++){
       if(board[i][j] == 0){
         printf("-1");
         return 0;
       }
-
-      if(max < board[i][j])
-        max = board[i][j];
     }
   }
   printf("%d",max-1);
