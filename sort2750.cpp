ebce0ed30152cbbe44b5a76e7b614c0b7be247ba
KimHyunSeok <sfsfkj@gmail.com>
2019-08-22 18:37:24 +0900

    change

--- sort2750.cpp
+++ sort2750.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 bool cmp(int a, int b){
-  return a<b;
+        return a<b;
 }
 int main(){
-  int N;
-  int * arr;
-  scanf("%d", &N);
-  arr = new int[N];
-  for(int i = 0 ; i < N ; i++){
-    scanf("%d",&arr[i]);
-  }
-  sort(arr,arr+N,cmp);
-  for(int i = 0 ; i < N ; i++){
-    printf("%d\n",arr[i]);
-  }
-  free(arr);
-  return 0;
+        int N;
+        int * arr;
+        scanf("%d", &N);
+        arr = new int[N];
+        for(int i = 0; i < N; i++) {
+                scanf("%d",&arr[i]);
+        }
+        sort(arr,arr+N,cmp);
+        for(int i = 0; i < N; i++) {
+                printf("%d\n",arr[i]);
+        }
+        free(arr);
+        return 0;
 }
