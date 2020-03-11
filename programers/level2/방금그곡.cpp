46842160828182c4d92de2dc9c5779d7013ca286
KimHyunSeok <sfsfkj@gmail.com>
2020-03-11 15:12:21 +0900

    200310

--- programers/level2/방금그곡.cpp
+++ programers/level2/방금그곡.cpp
@@ -3,71 +3,74 @@
 #include <sstream>
 #include <iostream>
 #include <map>
-#include <utility>
 using namespace std;
-map<string, char> m;
+map<string, char> mm;
 
-int get_min(string start, string end){
-    int start_hour = stoi(start.substr(0,2)), end_hour = stoi(end.substr(0,2));
-    int start_min = stoi(start.substr(3,2)), end_min = stoi(end.substr(3,2));
-    if(start_hour > end_hour) end_hour = 24;
-    return (end_min-start_min) + (end_hour-start_hour)*60;
+int getMin(string start, string end){
+    int s_h = stoi(start.substr(0,2)), s_m = stoi(start.substr(3,2));
+    int e_h = stoi(end.substr(0,2)), e_m = stoi(end.substr(3,2));
+    e_m = e_m + (e_h-s_h)*60;
+    return e_m - s_m;
 }
-string change_str(string s){
-    s += " ";
-    string tmp = "";
-    for(int i = 0 ; i < s.size()-1 ; i++){
-        char a = s.at(i), b = s.at(i+1);
-        string st = "";
-        if(b == '#'){
-            st += a;
-            st += b;
-            tmp += m[st];
+
+string getM(string s){
+    int len = s.size();
+    string result = "";
+    for(int i = 0 ; i < len ; i++){
+        if(i+1 < len && s.at(i+1) == '#'){
+            string tmp = s.substr(i,2);
+            result += mm[tmp];
             i++;
         }else {
-            st += a;
-            tmp += m[st];
+            string tmp = s.substr(i,1);
+            result += mm[tmp];
         }
     }
-    return tmp;
+    return result;
 }
-string solution(string my, vector<string> musicinfos) {
-    string val[] = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
-    for(char i = 0 ; i < 12 ; i++){
-        m[val[i]] = 'a'+i;
+
+string refineM(int time, string s){
+    int len = s.size();
+    string result = "";
+    int cnt = 0;
+    for(int i = 0 ; i < time ; i++){
+        if(cnt == len) cnt = 0;
+        result += s.at(cnt);
+        cnt++;
     }
-    my = change_str(my);
-    // cout<<my<<endl;
-    pair<string, int> answer = make_pair("(None)",0);
-    for(auto s : musicinfos){
-        vector<string> tmp;
-        stringstream ss(s);
-        string buf = "", refine = "";
+    return result;
+}
+
+string solution(string m, vector<string> musicinfos) {
+    char c = 'a';
+    vector<string> input{"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
+    for(auto& s : input){
+        mm[s] = c;
+        c++;
+    }
+    string answer = "";
+    string buf;
+    string refine_m = getM(m);
+    int conditionTime = 0, m_len = refine_m.size();
+    for(auto& musicin : musicinfos){
+        vector<string> v;
+        stringstream ss(musicin);
         while(getline(ss,buf,',')){
-            tmp.push_back(buf);
+            v.push_back(buf);
         }
-        int min = get_min(tmp[0], tmp[1]);
-        int c = 0, index = 0;
-        while(index < min){
-            char b_c = buf.at(c);
-            c++;
-            refine += b_c;
-            if(c == buf.size()) c = 0;
-            if(b_c != '#') index++;
-        }
-        refine = change_str(refine);
-        // cout<<"refine : "<<refine<<endl;
-        int len = refine.size() - my.size();
-        for(int i = 0 ; i <= len ; i++){
-            string sub = refine.substr(i,my.size());
-            if(my == sub){
-                if(answer.second < min){
-                    answer.first = tmp[2];
-                    answer.second = min;
-                    break;
+        int time = getMin(v[0],v[1]);
+        string refine_s = getM(v[3]);
+        refine_s = refineM(time,refine_s);
+        for(int i = 0 ; i <= (int)refine_s.size()-m_len ; i++){
+            string tmp_s = refine_s.substr(i,m_len);
+            if(tmp_s == refine_m){
+                if(conditionTime < time){
+                    answer = v[2];
+                    conditionTime = time;
                 }
             }
         }
     }
-    return answer.first;
+    if(answer.size() == 0) answer = "(None)";
+    return answer;
 }
