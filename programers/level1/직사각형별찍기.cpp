62d1d4c8dc4b36e761d9493b7ffc367b5073a6f9
KimHyunSeok <sfsfkj@gmail.com>
2020-03-26 16:05:05 +0900

    200327

---
+++ programers/level1/직사각형별찍기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d",&a, &b);
+    for(int i = 0 ; i < b ; i++){
+        for(int j = 0 ; j < a ; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
