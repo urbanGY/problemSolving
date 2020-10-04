8df0039bb92577169ef69f292c85086e7587a1f0
KimHyunSeok <sfsfkj@gmail.com>
2020-10-04 13:58:41 +0900

    상근이의 여행 크루스칼

---
+++ baekjoon/kruskal_9372.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+vector<int> node;
+
+int find(int n) {
+  if(n == node[n]) return n;
+  else return node[n] = find(node[n]);
+}
+
+int main() {
+  int T;
+  scanf("%d",&T);
+  for(int t = 0 ; t < T ; t++) {
+      int N, M, cnt = 0;
+      scanf("%d %d",&N,&M);
+      node = vector<int>(N+1);
+      for(int i = 1 ; i <= N ; i++) node[i] = i;
+      for(int i = 0 ; i < M ; i++) {
+        int a, b;
+        scanf("%d %d",&a,&b);
+        int from = find(a), to = find(b);
+        if(from != to) {
+          cnt++;
+          node[from] = to;
+        }
+      }
+      printf("%d\n",cnt);
+  }
+  return 0;
+}
